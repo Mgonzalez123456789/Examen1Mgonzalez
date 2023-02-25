5b35e19ad27a712baf6f004c95dbb3ef44daa363
Mgonzalez123456789 <124331491+Mgonzalez123456789@users.noreply.github.com>
2023-02-25 02:35:19 -0600

    ejercicio2

--- ExamenPrimerParcial/ExamenPrimerParcial/Ejercicio2.Designer.cs
+++ ExamenPrimerParcial/ExamenPrimerParcial/Ejercicio2.Designer.cs
@@ -33,113 +33,115 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
             this.N1textBox1 = new System.Windows.Forms.TextBox();
             this.N2textBox2 = new System.Windows.Forms.TextBox();
             this.N3textBox3 = new System.Windows.Forms.TextBox();
-            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(336, 54);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(106, 24);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "FACTURA";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(317, 57);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(241, 128);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(106, 24);
+            this.label2.Size = new System.Drawing.Size(77, 20);
             this.label2.TabIndex = 1;
-            this.label2.Text = "FACTURA";
+            this.label2.Text = "Producto:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(225, 126);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(241, 182);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(73, 18);
+            this.label3.Size = new System.Drawing.Size(57, 20);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Producto:";
+            this.label3.Text = "Precio:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(225, 164);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(241, 228);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(55, 18);
+            this.label4.Size = new System.Drawing.Size(77, 20);
             this.label4.TabIndex = 3;
-            this.label4.Text = "Precio:";
+            this.label4.Text = "Cantidad:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(225, 209);
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(241, 286);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(84, 18);
+            this.label5.Size = new System.Drawing.Size(91, 20);
             this.label5.TabIndex = 4;
             this.label5.Text = "Descuento:";
+            // 
+            // N1textBox1
+            // 
+            this.N1textBox1.Location = new System.Drawing.Point(342, 130);
+            this.N1textBox1.Name = "N1textBox1";
+            this.N1textBox1.Size = new System.Drawing.Size(100, 20);
+            this.N1textBox1.TabIndex = 5;
+            // 
+            // N2textBox2
+            // 
+            this.N2textBox2.Location = new System.Drawing.Point(340, 184);
+            this.N2textBox2.Name = "N2textBox2";
+            this.N2textBox2.Size = new System.Drawing.Size(100, 20);
+            this.N2textBox2.TabIndex = 6;
+            // 
+            // N3textBox3
+            // 
+            this.N3textBox3.Location = new System.Drawing.Point(342, 228);
+            this.N3textBox3.Name = "N3textBox3";
+            this.N3textBox3.Size = new System.Drawing.Size(100, 20);
+            this.N3textBox3.TabIndex = 7;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(269, 289);
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(351, 286);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(59, 18);
-            this.label6.TabIndex = 5;
-            this.label6.Text = "TOTAL:";
+            this.label6.Size = new System.Drawing.Size(41, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "15%";
             // 
-            // N1textBox1
+            // button1
             // 
-            this.N1textBox1.Location = new System.Drawing.Point(323, 127);
-            this.N1textBox1.Name = "N1textBox1";
-            this.N1textBox1.Size = new System.Drawing.Size(100, 20);
-            this.N1textBox1.TabIndex = 6;
-            // 
-            // N2textBox2
-            // 
-            this.N2textBox2.Location = new System.Drawing.Point(323, 165);
-            this.N2textBox2.Name = "N2textBox2";
-            this.N2textBox2.Size = new System.Drawing.Size(100, 20);
-            this.N2textBox2.TabIndex = 7;
-            // 
-            // N3textBox3
-            // 
-            this.N3textBox3.Location = new System.Drawing.Point(344, 289);
-            this.N3textBox3.Name = "N3textBox3";
-            this.N3textBox3.Size = new System.Drawing.Size(100, 20);
-            this.N3textBox3.TabIndex = 9;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(329, 209);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(37, 18);
-            this.label7.TabIndex = 10;
-            this.label7.Text = "15%";
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(287, 326);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 31);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "TOTAL";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_ClickAsync);
             // 
             // Ejercicio2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.N3textBox3);
             this.Controls.Add(this.N2textBox2);
             this.Controls.Add(this.N1textBox1);
-            this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -159,10 +161,10 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox N1textBox1;
         private System.Windows.Forms.TextBox N2textBox2;
         private System.Windows.Forms.TextBox N3textBox3;
-        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
     }
 }
--- ExamenPrimerParcial/ExamenPrimerParcial/Ejercicio2.cs
+++ ExamenPrimerParcial/ExamenPrimerParcial/Ejercicio2.cs
@@ -16,5 +16,26 @@ namespace ExamenPrimerParcial
         {
             InitializeComponent();
         }
+
+        private async Task button1_ClickAsync(object sender, EventArgs e)
+        {
+            decimal Precio = Convert.ToDecimal(N2textBox2.Text);
+            decimal Cantidad = Convert.ToDecimal(N3textBox3.Text);
+
+            decimal resultado = await FacturaAsyn(Precio, Cantidad);
+
+            MessageBox.Show($"El Total A Pagar es: {resultado}");
+        }
+
+        private async Task<decimal> FacturaAsyn(decimal precio,decimal cantidad)
+        {
+            decimal total = await Task.Run(() =>
+            {
+                decimal compra = precio * cantidad;
+                return compra * 15 / 100;
+            });
+         return total;
+        }
     }
 }
+
